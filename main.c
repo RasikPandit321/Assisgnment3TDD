885f9114acb99f402ff1a4c1be0b5148499e5b46
Rasik Pandit <Rpandit3867@conestogac.on.ca>
2024-10-31 15:34:59 -0400

    Player1won(PaperandRock)

--- main.c
+++ main.c
@@ -8,4 +8,45 @@ const char* winner(const char* Player1, const char* Player2)
 	{
 		return"Player1 won the match";
 	}
+	//code to check if Player1 chooses Rock and Player2 chooses Paper then Player2 will win
+	if (strcmp(Player1, "Rock") == 0 && strcmp(Player2, "Paper") == 0)
+	{
+		return"Player2 won the match";
+	}
+	//code to check if Player1 chooses Rock and Player2 chooses Rock then Draw
+	if (strcmp(Player1, "Rock") == 0 && strcmp(Player2, "Rock") == 0)
+	{
+		return"Draw";
+	}
+	//code to check if Player1 chooses Scissors and Player2 chooses Rock then Player2 will win
+	if (strcmp(Player1, "Scissors") == 0 && strcmp(Player2, "Rock") == 0)
+	{
+		return"Player2 won the match";
+	}
+	//code to check if Player1 chooses Scissors and Player2 chooses Paper then Player2 will win
+	if (strcmp(Player1, "Scissors") == 0 && strcmp(Player2, "Paper") == 0)
+	{
+		return"Player1 won the match";
+	}
+	//code to check if Player1 chooses Scissors and Player2 chooses Scissors then Player2 will win
+	if (strcmp(Player1, "Scissors") == 0 && strcmp(Player2, "Scissors") == 0)
+	{
+		return"Draw";
+	}
+	//code to check if Player1 chooses Paper and Player2 chooses Rock then Player2 will win
+	if (strcmp(Player1, "Paper") == 0 && strcmp(Player2, "Rock") == 0)
+	{
+		return"Player1 won the match";
+	}
+	//code to check if Player1 chooses Paper and Player2 chooses Scissors then Player2 will win
+	if (strcmp(Player1, "Paper") == 0 && strcmp(Player2, "Scissors") == 0)
+	{
+		return"Player2 won the match";
+	}
+	//code to check if Player1 chooses Paper and Player2 chooses Paper then Player2 will win
+	if (strcmp(Player1, "Paper") == 0 && strcmp(Player2, "Paper") == 0)
+	{
+		return"Draw";
+	}
+
 }
