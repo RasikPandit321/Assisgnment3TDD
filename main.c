f7556aa0d71ba6662809912fd5a9499a4eca047c
Rasik Pandit <Rpandit3867@conestogac.on.ca>
2024-10-31 15:46:22 -0400

    Draw(ScissorsandScissors)

--- main.c
+++ main.c
@@ -43,4 +43,9 @@ const char* winner(const char* Player1, const char* Player2)
 	{
 		return"Player1 won the match";
 	}
+	//code to check if Player1 chooses Scissors and Player2 chooses Scissors then Player2 will win
+	if (strcmp(Player1, "Scissors") == 0 && strcmp(Player2, "Scissors") == 0)
+	{
+		return"Draw";
+	}
 }  
