dda8c2c05bb933c388321d004419771dd6a1e794
Rasik Pandit <Rpandit3867@conestogac.on.ca>
2024-10-31 15:26:17 -0400

    Player1won (Rock and Scissors)

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
 #include<string.h>
+
+
+const char* winner(const char* Player1, const char* Player2)
+{
+	//code to check if Player1 chooses Rock and Player2 chooses Scissors then Player1 will win
+	if (strcmp(Player1, "Rock") ==0 && strcmp(Player2, "Scissors") ==0)
+	{
+		return"Player1 won the match";
+	}
+}
